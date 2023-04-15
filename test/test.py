6bbdef65a18e9740096c388897a1643d8861b0eb
Aymen Hmani <47940043+TheLime1@users.noreply.github.com>
2023-04-15 18:12:09 +0100

    future bug proof

--- test/test.py
+++ test/test.py
@@ -4,9 +4,9 @@ import sys
 
 # send a message and stream the response
 
-token = ""  # cookie token
+token = "CmpguqkTtuqLZh5w1XeRGw%3D%3D"  # cookie token
 client = poe.Client(token)
 
-message = input("type your message here\n")
+message = input("name me all the alphabets\n")
 for chunk in client.send_message("capybara", message, with_chat_break=True):
     print(chunk["text_new"], end="", flush=True)
