00cb84c474544e09a9a8615bde16d19064af5a70
Aymen Hmani <47940043+TheLime1@users.noreply.github.com>
2023-04-15 18:35:02 +0100

    Revert "future bug proof"
    This reverts commit 6bbdef65a18e9740096c388897a1643d8861b0eb.

--- snippets/test.py
+++ snippets/test.py
@@ -4,9 +4,9 @@ import sys
 
 # send a message and stream the response
 
-token = "CmpguqkTtuqLZh5w1XeRGw%3D%3D"  # cookie token
+token = ""  # cookie token
 client = poe.Client(token)
 
-message = input("name me all the alphabets\n")
+message = input("type your message here\n")
 for chunk in client.send_message("capybara", message, with_chat_break=True):
     print(chunk["text_new"], end="", flush=True)
